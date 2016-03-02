5c8eebab89b9d8db7bd9686a46e57b058b29956e
anandonline01 <anandonline01@gmail.com>
2016-03-02 18:57:08 +0530

    hh

--- src/main/java/com/javahash/spring/controller/HelloWorldController.java
+++ src/main/java/com/javahash/spring/controller/HelloWorldController.java
@@ -11,7 +11,7 @@ public class HelloWorldController {
     @RequestMapping("/hello")
     public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", "Anand Welcome");
-        return "helloworld";
+        return "helloworld"
     }
 
 }
